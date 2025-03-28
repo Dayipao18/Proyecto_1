f700fffe24a8f98b8ea020021b3cce9c1d441d6c
Dayipao18 <vasquezdayana@hotmail.com>
2025-03-27 19:28:52 -0500

    Se agrega opcion nueva

--- cajero.py
+++ cajero.py
@@ -52,7 +52,7 @@ while True:
     seleccion_cliente = lista_nombres[seleccion]
     if seleccion in range(len(lista_nombres)):
         while True:
-            operaciones = int(input('¿Qué quiere hacer?:\n0: Ver\n1: Retirar\n2: Consignar\n3: SALIR\n'))
+            operaciones = int(input('¿Qué quiere hacer?:\n0: Ver\n1: Retirar\n2: Consignar\n3: Transferir\n4 SALIR\n'))
             saldo_cuenta_usuario = dict_users[seleccion_cliente]
             # Ver
             if operaciones == 0:
